8e674a6f47b835bb224ca54c452a2d1f4d9588f1
AlexSpeal <alexspeal@gmail.com>
2024-03-23 21:11:44 +0300

    коммит

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see howdsds
+        // Press Alt+Enter with your caret at the highlighted text to see howdsd
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
