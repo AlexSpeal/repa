96ae6907da809aea86b606e308262aa7ba68caad
AlexSpeal <alexspeal@gmail.com>
2024-03-24 23:47:53 +0300

    тест

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        // IntelliJ IDEA suggests fxing it.
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click throw button in the gutter to run the code dfdfdf.
